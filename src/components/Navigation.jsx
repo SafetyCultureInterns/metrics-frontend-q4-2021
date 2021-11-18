50aca68557d82e32b49532207b96b433a9eba5db
Rory Phillips <rory.phillips@safetyculture.io>
2021-11-18 16:59:31 +1000

    [INIT]: Remove menu icon

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -13,15 +13,6 @@ export function Navigation() {
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         Metrics Frontend
                     </Typography>
