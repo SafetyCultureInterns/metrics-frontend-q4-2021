e17b56ca334524078cc7dfb0e67098ea460f3558
Rory Phillips <rory.phillips@safetyculture.io>
2021-11-18 16:58:49 +1000

    [INIT-1-2]: Added padding to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {BrowserRouter} from "react-router-dom";
 import styled from "styled-components"
+import { Box } from "@mui/material";
 
 import './App.css';
 import {MetricsRouter} from "./Router";
@@ -11,7 +12,11 @@ function App() {
         <StyledContainer maxWidth="lg">
             <BrowserRouter>
                 <Navigation/>
-                <MetricsRouter/>
+                <Box sx={{
+                    padding: '1rem'
+                }}>
+                    <MetricsRouter/>
+                </Box>
             </BrowserRouter>
         </StyledContainer>
     );
