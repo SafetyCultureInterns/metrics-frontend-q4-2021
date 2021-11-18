691d4ad161f5e68baf8091bfebd35177fd366b21
Rory Phillips <rory.phillips@safetyculture.io>
2021-11-18 16:57:48 +1000

    [INIT-1-2]: Added an authenticated request for context

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,29 @@
-import React from "react"
+import { useState, useEffect } from "react"
+import axios from "axios";
+import { useAuth } from "../hooks/auth";
 
 export const Home = () => {
-    return (<>You are home</>)
+    const [, { authenticatedRequest }] = useAuth();
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [context, setContext] = useState(null);
+
+    useEffect(() => {
+        authenticatedRequest(async (token) => {
+            const { data } = await axios.get('/auth/context', {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
+            setContext(data);
+            setIsLoading(false);
+        });
+    }, []);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    return (<>Welcome to the app {context.account_name}</>);
 }
